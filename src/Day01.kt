4a94731a97303366868ca5bf61a04299bd5b0e39
Francisco Sanchez <frango9000@gmail.com>
2022-12-01 21:07:05 +0100

    Day 1 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,14 +9,17 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return partitionByLine(input).map { it ->
+            it.map { it.toInt() }.fold(0) { acc, calories -> acc + calories }
+        }
+            .sortedDescending().take(3).reduce { acc, curr -> acc + curr }
     }
 
     val testInput = readInput("Day01_test")
-    check(part1(readInput("Day01_test")) == 24000)
-//    check(part2(testInput) == 2)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
