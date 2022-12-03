4b7957cf02e9a37ab8c639acb8c6f4ba93be075b
Francisco Sanchez <frango9000@gmail.com>
2022-12-03 11:31:15 +0100

    Day 3 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,19 +1,21 @@
 fun main() {
-    fun part1(line: List<String>): Int {
-        return line.map { it.chunked(it.length / 2) }
+    fun part1(input: List<String>): Int {
+        return input.map { it.chunked(it.length / 2) }
             .map { it[0].filter { x -> it[1].contains(x) }.toCharArray().first() }
             .sumOf { it.code - if (it.isUpperCase()) 38 else 96 }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input.chunked(3).map {
+            it[0].filter { x -> it[1].contains(x) && it[2].contains(x) }.toCharArray().first()
+        }.sumOf { it.code - if (it.isUpperCase()) 38 else 96 }
     }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
