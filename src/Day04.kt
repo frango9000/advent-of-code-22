0b07232374a6950e78d1fdadd706e6eb1f038403
Francisco Sanchez <frango9000@gmail.com>
2022-12-05 10:48:18 +0100

    Day 4 Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,19 +3,24 @@ fun main() {
         return input.filter { it: String ->
             val (a1, a2, b1, b2) = it.split(",")
                 .map { it.split("-").map { point -> point.toInt() } }.flatten()
-            a1 >= b1 && a2 <= b2 || b1 >= a1 && b2 <= a2
+            a1 in b1..b2 || b2 in a1..a2
         }.size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input.filter { it: String ->
+            val (a1, a2, b1, b2) = it.split(",")
+                .map { it.split("-").map { point -> point.toInt() } }.flatten()
+            a1 in b1..b2 || b1 in a1..a2
+
+        }.size
     }
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
