e063f019d564964e5b8e8de0d388b5874a33b8db
Francisco Sanchez <frango9000@gmail.com>
2022-12-06 19:57:28 +0100

    Day 6 Part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,16 +1,11 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        val (dataStream) = input
-        for (i in 3 until dataStream.length) {
-            if (setOf(dataStream[i - 3], dataStream[i - 2], dataStream[i - 1], dataStream[i]).size == 4) {
-                return i + 1
-            }
-        }
-        return 0
+        return uniqueSequenceIndex(input, 4)
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return uniqueSequenceIndex(input, 14)
     }
 
     val testInput = readInput("Day06_test")
@@ -19,9 +14,23 @@ fun main() {
     check(part1(listOf("nppdvjthqldpwncqszvftbrmjlhg")) == 6)
     check(part1(listOf("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")) == 10)
     check(part1(listOf("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")) == 11)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 19)
+    check(part2(listOf("bvwbjplbgvbhsrlpgdmjqwftvncz")) == 23)
+    check(part2(listOf("nppdvjthqldpwncqszvftbrmjlhg")) == 23)
+    check(part2(listOf("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")) == 29)
+    check(part2(listOf("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")) == 26)
 
     val input = readInput("Day06")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
+}
+
+fun uniqueSequenceIndex(input: List<String>, count: Int): Int {
+    val (dataStream) = input
+    for (i in count - 1 until dataStream.length) {
+        if ((i - count + 1..i).toList().map { dataStream[it] }.toSet().size == count) {
+            return i + 1
+        }
+    }
+    return 0
 }
