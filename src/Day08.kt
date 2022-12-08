016c50a0a5b11984a380995e524f7ceb54b0b304
Francisco Sanchez <frango9000@gmail.com>
2022-12-08 15:40:50 +0100

    Day 8 Part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -45,14 +45,60 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val forest = input.map { it.split("").filter { c -> c.isNotEmpty() }.map { character -> character.toInt() } }
+        val forestOfScores = input.map { it.map { 0 }.toMutableList() }
+
+        for (i in forest.indices) {
+            if (i == 0 || i == forest.size - 1) continue
+            for (j in forest[i].indices) {
+                if (j == 0 || j == forest[i].size - 1) continue
+                var leftDistance = 0
+                for (j2 in (j - 1 downTo 0)) {
+                    leftDistance++
+                    if (forest[i][j2] >= forest[i][j]) {
+                        break
+                    }
+                }
+                if (leftDistance == 0) continue
+
+                var rightDistance = 0
+                for (j2 in (j + 1 until forest[i].size)) {
+                    rightDistance++
+                    if (forest[i][j2] >= forest[i][j]) {
+                        break
+                    }
+                }
+                if (rightDistance == 0) continue
+
+                var topDistance = 0
+                for (i2 in (i - 1 downTo 0)) {
+                    topDistance++
+                    if (forest[i2][j] >= forest[i][j]) {
+                        break
+                    }
+                }
+                if (topDistance == 0) continue
+
+                var botDistance = 0
+                for (i2 in (i + 1 until forest.size)) {
+                    botDistance++
+                    if (forest[i2][j] >= forest[i][j]) {
+                        break
+                    }
+                }
+                if (botDistance == 0) continue
+                forestOfScores[i][j] = leftDistance * rightDistance * topDistance * botDistance
+            }
+        }
+
+        return forestOfScores.flatten().max()
     }
 
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 21)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 8)
 
     val input = readInput("Day08")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
