7f2944fc297d1631fec1fe63e3aa5ed9dd6ade4c
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2022-12-18 19:46:40 +0100

    Day 15 part 2 too slow

--- src/Day15.kt
+++ src/Day15.kt
@@ -3,43 +3,67 @@ import kotlin.math.absoluteValue
 fun main() {
     val input = readInput("Day15")
     printTime { print(Day15.part1(input, 2_000_000)) }
-//    println(Day15.part2(input))
+//    printTime { println(Day15.part2(input, 4_000_000)) }
 }
 
 class Day15 {
     companion object {
-        fun part1(input: List<String>, yTarget: Int): Int {
+        fun part1(input: List<String>, targetY: Int): Int {
             val sensorsAndBeacons = input.map {
                 it.substring(10).split(": closest beacon is at ").map { it.split(", ").map { it.substring(2).toInt() } }
                     .map { (x, y) -> Point(x, y) }
             }
 
-            val maxCoverage = sensorsAndBeacons.maxOf { it[0].rectilinearDistanceTo(it[1]) }
+            val offset = sensorsAndBeacons.maxOf { it[0].rectilinearDistanceTo(it[1]) } * 2
             val beaconMaxX = sensorsAndBeacons.maxOf { it[1].x }
-            val offset = maxCoverage * 2
-            val xsInYTarget = BooleanArray(beaconMaxX + offset) { true }
+            val blockedXsInTargetY = BooleanArray(beaconMaxX + offset)
 
             for ((sensor, beacon) in sensorsAndBeacons) {
                 val sensorCoverage = sensor.rectilinearDistanceTo(beacon)
-                val yDistanceToYTarget = (yTarget - sensor.y).absoluteValue
+                val yDistanceToYTarget = (targetY - sensor.y).absoluteValue
                 val yOverlapBy = sensorCoverage - yDistanceToYTarget
                 if (yOverlapBy > 0) {
                     for (x in (sensor.x - (yOverlapBy)) towards (sensor.x + (yOverlapBy))) {
-                        xsInYTarget[x + offset] = false
+                        blockedXsInTargetY[x + offset] = true
                     }
                 }
             }
 
-            val beaconXsInYTarget = sensorsAndBeacons.map { it[1] }.filter { it.y == yTarget }.map { it.x }
+            val beaconXsInYTarget = sensorsAndBeacons.map { it[1] }.filter { it.y == targetY }.map { it.x }
             for (beaconX in beaconXsInYTarget) {
-                xsInYTarget[beaconX + offset] = true
+                blockedXsInTargetY[beaconX + offset] = false
             }
 
-            return xsInYTarget.count { !it }
+            return blockedXsInTargetY.count { it }
         }
 
-        fun part2(input: List<String>): Int {
-            return 0
+        fun part2(input: List<String>, side: Int): Int {
+            val sensorsAndBeacons = input.map {
+                it.substring(10).split(": closest beacon is at ").map { it.split(", ").map { it.substring(2).toInt() } }
+                    .map { (x, y) -> Point(x, y) }
+            }
+            val validRange = 0..side
+            val blockedXsMap = (validRange.map { BooleanArray(side + 1) }).toTypedArray()
+
+            for ((sensor, beacon) in sensorsAndBeacons) {
+                val sensorCoverage = sensor.rectilinearDistanceTo(beacon)
+                for (y in ((sensor.y - sensorCoverage) towards (sensor.y + sensorCoverage))) {
+                    val yDistanceToYTarget = (y - sensor.y).absoluteValue
+                    val yOverlapBy = sensorCoverage - yDistanceToYTarget
+                    for (x in (sensor.x - (yOverlapBy)) towards (sensor.x + (yOverlapBy))) {
+                        if (x in validRange && y in validRange) blockedXsMap[y][x] = true
+                    }
+                }
+            }
+
+            for ((x, y) in sensorsAndBeacons.map { it[1] }) {
+                if (x in validRange && y in validRange) blockedXsMap[y][x] = true
+            }
+
+            val (y, x) = blockedXsMap.mapIndexed { index, booleans -> listOf(index, booleans.indexOfFirst { !it }) }
+                .first { it[1] >= 0 }
+
+            return x * 4_000_000 + y
         }
     }
 
@@ -52,5 +76,8 @@ class Day15 {
         override fun toString(): String {
             return "Point(x=$x, y=$y)"
         }
+
+        operator fun component1() = x
+        operator fun component2() = y
     }
 }
